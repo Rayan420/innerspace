64a221e184c3db1312c8b3f25db2fba4a198f322
Adil Khader <adelabdulrahman0@gmail.com>
2024-01-23 20:10:03 +0300

    test

--- lib/bloc/sign_up_bloc/sign_up_bloc.dart
+++ lib/bloc/sign_up_bloc/sign_up_bloc.dart
@@ -15,6 +15,7 @@ class SignUpBloc extends Bloc<SignUpEvent, SignUpState> {
       emit(SignUpProcess());
       try {
         MyUser user = await _userRepository.signUp(event.user, event.password);
+        print("user signed up - sign up bloc");
         await _userRepository.setUserData(user);
         print("user data set - sign up bloc");
         emit(SignUpSuccess());
